c0833b178fb3ba0be4f0e781c7fc0e405f3f2ca2
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2019-10-08 16:04:53 -0300

    add linear regression module

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -1,2 +1,65 @@
+import math
+
+import numpy
+from matplotlib import pyplot
+
+
 def hypoteses(theta0, theta1, x):
-    return theta0 + (theta1*x)
+    return theta0 + (theta1 * x)
+
+
+def cost(theta0, theta1, X, y):
+    # RMSE
+    cost_value = 0
+    for (xi, yi) in zip(X, y):
+        cost_value += 0.5 * ((hypoteses(theta0, theta1, xi) - yi) ** 2)
+    return math.sqrt(cost_value)
+
+
+def plot_line(theta0, theta1, X, y):
+    max_x = numpy.max(X) + 100
+    min_x = numpy.min(X) - 100
+
+    xplot = numpy.linspace(min_x, max_x, 1000)
+    yplot = theta0 + theta1 * xplot
+    pyplot.plot(xplot, yplot, color="#ff0000", label="Regression Line")
+    pyplot.scatter(X, y)
+    pyplot.axis([-10, 10, 0, 200])
+    pyplot.show()
+
+
+def derivatives(theta0, theta1, X, y):
+    dtheta0 = 0
+    dtheta1 = 0
+
+    for xi, yi in zip(X, y):
+        dtheta0 += hypoteses(theta0, theta1, xi) - yi
+        dtheta1 += (hypoteses(theta0, theta1, xi) - yi) * xi
+
+    dtheta0 /= len(X)
+    dtheta1 /= len(X)
+
+    return dtheta0, dtheta1
+
+
+def update_parameters(theta0, theta1, X, y, alpha=0.005):
+    dtheta0, dtheta1 = derivatives(theta0, theta1, X, y)
+    theta0 = theta0 - (alpha * dtheta0)
+    theta1 = theta1 - (alpha * dtheta1)
+
+    return theta0, theta1
+
+
+def fit(X, y):
+    theta0 = numpy.random.rand()
+    theta1 = numpy.random.rand()
+    for i in range(0, 1000):
+        theta0, theta1 = update_parameters(theta0, theta1, X, y, 0.005)
+    return theta0[0], theta1[0]
+
+
+def generate_model(theta0, theta1):
+    def model(x, theta0=theta0, theta1=theta1):
+        return hypoteses(theta0, theta1, x)
+
+    return model
--- test/test_linear_regression.py
+++ test/test_linear_regression.py
@@ -1,8 +1,48 @@
 from linear_regression import linear_regression
 
+
 def test_hypoteses_function():
     theta0 = 1
     thtea1 = 2
     x = 4.5
     y = linear_regression.hypoteses(theta0, thtea1, x)
     assert y == 10.0
+
+
+def test_cost_function():
+    X = [1, 2, 3, 4]
+    y = [4, 3, 2, 1]
+    theta0 = 1
+    theta1 = 2
+    assert linear_regression.cost(theta0, theta1, X, y) == 47
+
+
+def test_derivative():
+    X = [1, 2, 3, 4]
+    y = [4, 3, 2, 1]
+    theta0 = 1
+    theta1 = 2
+
+    dt0, dt1 = linear_regression.derivatives(theta0, theta1, X, y)
+    assert dt0 == 3.5
+    assert dt1 == 12.5
+
+
+def test_update_parameters():
+    X = [1, 2, 3, 4]
+    y = [4, 3, 2, 1]
+    theta0 = 1
+    theta1 = 2
+    alpha = 0.05
+
+    t0, t1 = linear_regression.update_parameters(theta0, theta1, X, y, alpha)
+    assert t0 == 0.825
+    assert t1 == 1.375
+
+
+def test_fit():
+    X = [1, 2, 3, 4]
+    y = [5, 6, 7, 8]
+    theta0, theta1 = linear_regression.fit(X, y)
+    assert theta0 > 0
+    assert theta1 > 0
